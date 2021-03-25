14e5b9c687d4ab5b2af0196aa7d778283e38385a
Anton Larkin <anton11061998@gmail.com>
2021-03-25 21:37:39 +0300

    init project

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    [SerializeField] int minimalValue = 1;
+    [SerializeField] int maximalValue = 1000;
+    int guessValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Guess the number from "  + minimalValue + " to " + maximalValue);
+        calculateNextPossibleNumber();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            minimalValue = guessValue;
+            calculateNextPossibleNumber();
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            maximalValue = guessValue;
+            calculateNextPossibleNumber();
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+        {
+            print("Great! Your numer is " + guessValue);
+        }
+    }
+
+    void calculateNextPossibleNumber()
+    {
+        guessValue = (minimalValue + maximalValue) / 2;
+        print("Is your number " + guessValue + "?");
+    }
+}
