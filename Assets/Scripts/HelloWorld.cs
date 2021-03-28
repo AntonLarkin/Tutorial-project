62bd3328652b32740154a67ef5a21fe13648d430
Anton Larkin <anton11061998@gmail.com>
2021-03-29 02:22:27 +0300

    add MagicNumber script, edit HelloWorld script

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -8,37 +8,79 @@ public class HelloWorld : MonoBehaviour
     [SerializeField] private int minimalValue = 1;
     [SerializeField] private int maximalValue = 1000;
     int guessValue;
+    int countGuessesQuantity = 0;
     [SerializeField] private Text label;
+    [SerializeField] private Text title;
+    [SerializeField] private Text instructions;
+
     // Start is called before the first frame update
     void Start()
     {
-        print("Guess the number from "  + minimalValue + " to " + maximalValue);
+        showTitleAndInstructions();
         calculateNextPossibleNumber();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+        {
+            endGame();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            minimalValue = 1;
+            maximalValue = 1000;
+            restartTheGame();
+            calculateNextPossibleNumber();
+        }
+        
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             minimalValue = guessValue;
             calculateNextPossibleNumber();
+            countGuessesQuantity++;
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             maximalValue = guessValue;
             calculateNextPossibleNumber();
+            countGuessesQuantity++;
         }
-        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
-        {
-            print("Great! Your numer is " + guessValue);
-        }
+        
+        
     }
 
     void calculateNextPossibleNumber()
     {
+
         guessValue = (minimalValue + maximalValue) / 2;
-        print("Is your number " + guessValue + "?");
-        label.text = "Is your number " + guessValue + "?";
+        label.text = "Загаданное вами число " + guessValue + "?";
+    }
+
+    /*void showTheNameOfTheGame()
+    {
+        title.text = "Let's guess the number from " + minimalValue + " to " + maximalValue;
+    }
+    void showInsruction()
+    {
+        instructions.text = "Press ArrowUp, if your number is higer, ArrowDown, if your number is lower. \n Press ENTER, if the number is correct.";
+    }*/
+    void showTitleAndInstructions()
+    {
+        title.text = "Давайте выберем число от " + minimalValue + " до " + maximalValue;
+        instructions.text = "Нажмите СТРЕЛКУ ВВЕРХ, если число больше того, что на экране;\nСТРЕЛКУ ВНИЗ, если число меньше того, что на экране.\n Нажмите ВВОД, если на экране ваше число.";
+    }
+    void endGame()
+    {
+        title.text = "Поздравляю!";
+        instructions.text = "Нажмите пробел, чтобы начать сначала!";
+        label.text = "Ваше число " + guessValue + "! \n Количество попыток " + countGuessesQuantity + "!";
+
+    }
+    void restartTheGame()
+    {
+        countGuessesQuantity = 0;
+        showTitleAndInstructions();
     }
 }
---
+++ Assets/Scripts/MagicNumers.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MagicNumers : MonoBehaviour
+{
+    [SerializeField] private Text label;
+    [SerializeField] private Text title;
+    [SerializeField] private Text instructions;
+
+    [SerializeField] int maxSum = 50;
+
+    int sum ;
+    int countPressedButtonsQuantity = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject Game2 = GetComponent<GameObject>();
+
+        showTitleAndInstructions();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        addValue();
+        ShowSum();
+        if (sum >= maxSum && Input.GetKeyDown(KeyCode.Space))
+        {
+            sum = 0;
+            restartTheGame();
+        }
+    }
+
+    void showTitleAndInstructions() 
+    {
+        title.text = "Давайте суммировать числа!";
+        instructions.text = "Нажмите клавишу 1-9 чтобы их просуммировать. Ваша цель достичь 50.";
+        label.text = "Текущая сумма 0! Нажмите цифру...";
+    }
+    void addValue()
+    {
+        if (sum >= maxSum ) 
+        { 
+            endGame();
+        }
+        
+        else if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1)) 
+        { 
+            sum += 1;
+            countPressedButtonsQuantity++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.Alpha2)) 
+        { 
+            sum += 2;
+            countPressedButtonsQuantity++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad3) || Input.GetKeyDown(KeyCode.Alpha3)) 
+        { 
+            sum += 3;
+            countPressedButtonsQuantity++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad4) || Input.GetKeyDown(KeyCode.Alpha4)) 
+        { 
+            sum += 4;
+            countPressedButtonsQuantity++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad5) || Input.GetKeyDown(KeyCode.Alpha5)) 
+        { 
+            sum += 5;
+            countPressedButtonsQuantity++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad6) || Input.GetKeyDown(KeyCode.Alpha6)) 
+        { 
+            sum += 6;
+            countPressedButtonsQuantity++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad7) || Input.GetKeyDown(KeyCode.Alpha7)) 
+        { 
+            sum += 7;
+            countPressedButtonsQuantity++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad8) || Input.GetKeyDown(KeyCode.Alpha8)) 
+        { 
+            sum += 8;
+            countPressedButtonsQuantity++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad9) || Input.GetKeyDown(KeyCode.Alpha9)) 
+        { 
+            sum += 9;
+            countPressedButtonsQuantity++;
+        }
+    }
+
+    void ShowSum()
+    {
+        if (sum < maxSum) 
+        {
+            label.text = "Текущая сумма " + sum + "!";
+        }
+        
+    }
+    void endGame()
+    {
+        title.text = "Поздравляю!";
+        instructions.text = "Нажмите ПРОБЕЛ, чтобы начать сначала!";
+        label.text = "Вы достигли 50! \n Количество слагаемых чисел " + countPressedButtonsQuantity + "!";
+    }
+    void restartTheGame()
+    {
+        countPressedButtonsQuantity = 0;
+        showTitleAndInstructions();
+    }
+}
